2948265423ac85e97f1779f97cd90e86a8a505b6
Vlad <kozhinv777@gmail.com>
2022-08-05 22:00:08 +0300

    Create City model

--- travel/views.py
+++ travel/views.py
@@ -4,5 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     name = 'Jack'
+    title = 'Home Page'
 
-    return render(request, 'home.html', {'first_name': name})
+    return render(request, 'home.html', {'first_name': name,
+                                         'page_title': title})
+
+
+def about(request):
+    word = 'About us'
+    title = 'About us'
+
+    return render(request, 'about.html', {'page_name': word,
+                                          'page_title': title})
